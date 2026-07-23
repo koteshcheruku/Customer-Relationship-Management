ccb62ff027b6e35955bce37b02fd0ab5bfe0b698
Kotesh Cheruku <koteshcheruku000@gmail.com>
2026-07-23 09:47:33 +0530

    Frontend +/api

--- my-crm-react-app/src/lib/api.js
+++ my-crm-react-app/src/lib/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 // Create an Axios instance
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
   headers: {
     "Content-Type": "application/json",
   },
